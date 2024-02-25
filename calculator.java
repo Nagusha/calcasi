b578fcbe33ba36c7a6ac1d6803aa1eebde557106
Administrator <administrator@Salauddins-MacBook-Pro.local>
2024-02-25 19:22:29 +0530

    addition

--- calculator.java
+++ calculator.java
@@ -1,17 +1,10 @@
-import java.util.Scanner;
-                 
-public class calculator
+import java.util.Scanner;           
+class calculator
 {
     public static void main(String args[])
-  {
-    int first, second, add, subtract;
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("Enter Two Numbers : ");
-    first = scanner.nextInt();
-    second = scanner.nextInt();
-    add = first + second;
-    subtract = first - second;
-    System.out.println("Sum = " + add);
-    System.out.println("Difference = " + subtract);
-  }
+    {
+        int A = 25, B = 25, sum;
+        sum = A + B;
+        System.out.println("the sum of two numbers is: "+sum);
+    }
 }
